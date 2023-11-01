da16f80272a726d4d94232a0d3c96ce4c3b6a9e7
PickolZi <pickol876@gmail.com>
2023-11-01 02:04:54 -0700

    Filters ebay seller inventory API calls into 2 batches in order to recieve more than 10000 items. Fixed bug where some ebay items are indicated as 'sold' but are not actually sold.

--- backend/api/ebay_api.py
+++ backend/api/ebay_api.py
@@ -9,8 +9,8 @@ from xmlToJson import xmlToJsonParser
 with open("../SECRETS.json", "r") as f:
     cred = json.load(f)
 
-all_item_ids = []
 EBAY_GET_ITEMS_ENDPOINT = "https://svcs.ebay.com/services/search/FindingService/v1"  # Returns in XML format because using old finding API.
+EBAY_SHOPPING_GET_ITEMS_ENDPOINT = "https://open.api.ebay.com/shopping"
 headers = {
         "X-EBAY-SOA-SECURITY-APPNAME": cred['appid'],
         "X-EBAY-SOA-OPERATION-NAME": None
@@ -21,17 +21,45 @@ def pretty_print_json(json_data):
     print(json.dumps(json_data, indent=5))
 
 
-def getAllEbayDataFromStores(store, page=1, output=[]):
+def getAllEbayDataFromStores(store, page=1, output=[], minPrice=None, maxPrice=None):
     # Gets all items an ebay store seller has.
     # params: store => ebay store name.
     # Returns list of json of ebay item data
+
+    if page==1:
+        output.clear()
+
+    if not isValidStore(store):
+        return False
+
     headers["X-EBAY-SOA-OPERATION-NAME"] = "findItemsIneBayStores"
 
     body = f"""<?xml version="1.0" encoding="UTF-8"?>
     <findItemsIneBayStoresRequest xmlns="http://www.ebay.com/marketplace/search/v1/services">
         <storeName>{store}</storeName>
-        <outputSelector>StoreInfo</outputSelector>
-        <paginationInput>
+        <outputSelector>StoreInfo</outputSelector>"""
+    
+    if minPrice:
+        body += f"""
+        <itemFilter>
+            <name>MinPrice</name>
+            <value>{minPrice}</value>
+            <paramName>Currency</paramName>
+            <paramValue>USD</paramValue>
+        </itemFilter>
+        """
+
+    if maxPrice:
+        body += f"""
+        <itemFilter>
+            <name>MaxPrice</name>
+            <value>{maxPrice}</value>
+            <paramName>Currency</paramName>
+            <paramValue>USD</paramValue>
+        </itemFilter>
+        """
+
+    body += f"""<paginationInput>
             <entriesPerPage>100</entriesPerPage>
             <pageNumber>{page}</pageNumber>
         </paginationInput>
@@ -42,18 +70,15 @@ def getAllEbayDataFromStores(store, page=1, output=[]):
 
     num_of_pages = int(json_data['findItemsIneBayStoresResponse']['paginationOutput']['totalPages'])
 
-    if page > num_of_pages:
-        return output
-
     ebay_items = json_data['findItemsIneBayStoresResponse']['searchResult']['item']
     for item in ebay_items:
         item_id = item['itemId']
-        title = item['title']
+        title = item['title'].replace("'", "")
         listed_date = item['listingInfo']['startTime'].replace("T", " ")[:-1]
         price = item['sellingStatus']['currentPrice']['#text']
         item_url = item['viewItemURL']
         image_url = item['galleryURL'].replace('l140', 'l1600')
-        location = item['location']
+        location = item['location'].replace("'", "")
         status = item['sellingStatus']['sellingState']
 
         output.append({
@@ -66,15 +91,70 @@ def getAllEbayDataFromStores(store, page=1, output=[]):
             'location': location,
             'status': status
         })
-
-    # print(item_url)
-    # return
     print(f"Got page {page} / {num_of_pages} from ebay API")
 
-    if page < 100:
-        getAllEbayDataFromStores(store, page=page+1, output=output)   
+    if page == num_of_pages or page == 100:
+        return output
 
-    return output
+    getAllEbayDataFromStores(store, page=page+1, output=output, minPrice=minPrice, maxPrice=maxPrice)
+    return output[:]
+
+def areSoldItems(total_ebay_ids):
+    # Given the ebay item ids, return if the item has been sold/completed.
+    # params: ebay_id => [int]
+    # returns: dictionary with the key being the ids and values being booleans.
+    results = {}
+
+    # API request
+    headers["X-EBAY-API-IAF-TOKEN"] = f"Bearer {cred['userToken']}"
+    headers["X-EBAY-API-SITE-ID"] = "0"
+    headers["X-EBAY-API-CALL-NAME"] = "GetItemStatus"
+    headers["X-EBAY-API-VERSION"] = "863"
+    headers["X-EBAY-API-REQUEST-ENCODING"] = "xml"
+
+    quotient = len(total_ebay_ids) // 20
+    remainder = len(total_ebay_ids) % 20
+    print(f"Calling API {quotient+1} times to see which items need to be removed...")
+    for i in range(quotient+1):
+        ebay_ids = total_ebay_ids[i*20:(i+1)*20]  # Can only do API calls in batches of 20
+        if i == quotient:
+            ebay_ids = total_ebay_ids[i*20:]
+
+        body = f"""<?xml version="1.0" encoding="utf-8"?>
+        <GetItemStatusRequest xmlns="urn:ebay:apis:eBLBaseComponents">"""
+
+        for ebay_id in ebay_ids:
+            body+= f"<ItemID>{ebay_id}</ItemID>"
+
+        body += "</GetItemStatusRequest>"
+
+        response = requests.post(EBAY_SHOPPING_GET_ITEMS_ENDPOINT, data=body, headers=headers)
+        json_data = xmlToJsonParser(response.text)["GetItemStatusResponse"]
+        # Handling Errors
+        RESPONSE_CODES = ["Success", "PartialFailure", "Failure"]
+        SERVER_RESPONSE_CODE = json_data["Ack"]
+
+        if SERVER_RESPONSE_CODE == RESPONSE_CODES[2]:
+            print("Failed to retrieve any sold ebay items.")
+            return False
+        elif SERVER_RESPONSE_CODE == RESPONSE_CODES[1]:
+            error_items = json_data["Errors"]["ErrorParameters"]["Value"].split(",")
+            results["Failures"] = error_items
+            print("Error when trying to find these ebay items:", error_items)
+
+        ebay_items = json_data["Item"]
+        if type(ebay_items) == dict:
+            ebay_items = [ebay_items]
+
+        # Key, Value: ebay id, boolean
+        for ebay_item in ebay_items:
+            item_id = ebay_item['ItemID']
+            status = ebay_item['ListingStatus']
+
+            results[item_id] = (status != "Active")
+
+    return results
+
 
 def isValidStore(store):
     # Given the store name, return if it's a valid store name on ebay or not.
@@ -100,8 +180,14 @@ def isValidStore(store):
 
 
 if __name__ == "__main__":
-    # ebayItems = getAllEbayDataFromStores("Basset Auto Wreckers")
-    print(isValidStore("PARTS THAT FIT LLC"))
-    # print(ebayItems)
-    # print(len(ebayItems))
-    # pass
+    # ebayItems = getAllEbayDataFromStores("Basset Auto Wreckers", minPrice=50)
+
+    x = [295778007334, 293691222982, 295705847685, 293582067774, 295459132336, 295464479330, 295478218835, 292727410224, 304804608243, 295791717743, 293120981956, 292713027082, 304500674113, 305214724014, 295705848289, 295329815173, 303477230115, 292713029569, 305124992789, 304792061331, 294279016350, 305078758459, 293567361848, 293792614665, 294749552524, 303968684831, 304973884654, 303908610702, 295025220445, 292745732411, 304902130710, 295646269195, 302871738062, 303561012266, 304129675761, 303795842298, 304903784487, 295961818315, 294227051986, 304201244737, 304736576101, 304379008928, 295453761114, 304698535151, 295704837569, 304834615878, 295900176773, 292732126285, 303252154280, 295089282585, 305004819413, 304924057588, 304229165193, 294029830457, 304829658454, 293957015254, 295555457840, 304897053580, 295743731292, 304944751849, 292943552901, 295567199095, 304758480640, 304914935097, 304813455855, 302932605797, 302883362428, 293721343139, 293995676248, 295038220376, 304959603170, 304555601837, 304140435740, 295769749473]
+
+    pretty_print_json(areSoldItems(x))
+
+    # print(isValidStore("PARTS THAT FIT LLC"))
+    # for ebayItem in ebayItems:
+    #     pretty_print_json(ebayItem)
+    # print("Total Items:", len(ebayItems))
+    pass
--- backend/api/ebay_functions.py
+++ backend/api/ebay_functions.py
@@ -2,7 +2,7 @@
 import sqlite3
 from datetime import datetime, timedelta
 
-from ebay_api import getAllEbayDataFromStores, isValidStore, pretty_print_json
+from ebay_api import getAllEbayDataFromStores, isValidStore, areSoldItems, pretty_print_json
 
 DATABASE = "../instance/db.sqlite3"
 
@@ -15,7 +15,7 @@ def getIdFromStoreName(store):
     """
 
     if not isValidStore(store):
-        return -1
+        return False
 
     con = sqlite3.connect(DATABASE)
     cur = con.cursor()
@@ -28,78 +28,25 @@ def getIdFromStoreName(store):
 
 def dailyUpdateDatabaseFromEbay(store):
     # Will be ran once a day.
-    # Calls getAllEbayDataFromStores() and gets every ebay item from a store and updates it to current database. If item already exists or there's a new item being added, the 'date_last_updated' date will be updated. However, this means, old items that aren't coming from the api call will not be updated. This mean the item was taken down or sold.
-    # params: store => String of ebay store id. From stores table.
+    # Calls getAllEbayDataFromStores(), gets every ebay item from a store and updates it to current database. If item already exists or there's a new item being added, the 'date_last_updated' date will be updated. However, this means, old items that aren't coming from the api call will not be updated. This mean the item was taken down or sold.
+    # params: store => String of ebay store name. From stores table.
     # Returns: Nothing. Updates database.
 
     if not isValidStore(store):
-        return -1
+        return False
 
-    ebay_items = getAllEbayDataFromStores(store)
     store_id = getIdFromStoreName(store)
 
-    # Connecting to database.
-    con = sqlite3.connect(DATABASE)
-    cur = con.cursor()
-
-    cur_updated_time = datetime.now() - timedelta(minutes=10)
-
-    for index, ebay_item in enumerate(ebay_items):
-        item_id = ebay_item['item_id']
-        title = ebay_item['title'].replace("'", "")
-        listed_date = ebay_item['listed_date']
-        price = ebay_item['price']
-        item_url = ebay_item['item_url']
-        image_url = ebay_item['image_url']
-        location = ebay_item['location'].replace("'", "")
-        status = ebay_item['status']
-
-        # Deletes ebay item if exists
-        cur.execute(f"""
-        DELETE FROM items WHERE item_id={item_id}
-        """)
-
-        sql_query = f"""
-        INSERT INTO items
-VALUES ({item_id}, '{title}', '{listed_date}', '{datetime.now()}', {price}, '{item_url}', '{image_url}', '{location}', '{status}', {store_id});
-        """
-        cur.execute(sql_query)
-        print(f"Inserted {index+1} / {len(ebay_items)} into items database. item id: {item_id}")
-        # print(sql_query)  # For debugging 
-        # print()  
-    print(f"Finished inserting {len(ebay_items)} into database.")  
-
-    # Update items that weren't updated recently, into "sold" status.
-    cur.execute(f"""
-    UPDATE items 
-    SET status="Sold"
-    WHERE date_last_updated<'{cur_updated_time} and store={store_id}'
-    """)
-
-    con.commit()
-    con.close()
-
-def updateSpecificPartFromEbayItems(store, attributes=[]):
-    # Calls getAllEbayDataFromStores() and will update attributes of the ebay item provided by the arguments. Will not update date_last_updated unless specified.
-    # params: [Strings] list of strings. Strings will be the attribute name of the items table to be updated.
-    # Return nothing. Updated database. 
-
-    sample_attributes = ['item_id', 'title', 'listed_date', 'date_last_updated', 'price', 'item_url', 'image_url', 'location', 'status', 'store_id']
-
-    if attributes == []:  # If empty, return.
-        print("empty attributes, please add database items attributes to attributes list.")
-        return -1
-
-    for attribute in attributes:  # If any of the arguments are not in the verified sample attributes, exit.
-        if attribute not in sample_attributes:
-            print(f"Attribute: {attribute} not in sample_attributes list.")
-            return -1
+    # For stores with more than 10k parts(100 entries for 100 pages). I can not call pages after 100, so I have to break up api call and filter by price so pages are less than 100.
+    ebay_items = []
+    ebay_items.extend(getAllEbayDataFromStores(store, maxPrice=49.99))
+    ebay_items.extend(getAllEbayDataFromStores(store, minPrice=50))
 
     # Connecting to database.
     con = sqlite3.connect(DATABASE)
     cur = con.cursor()
 
-    ebay_items = getAllEbayDataFromStores(store)
+    cur_updated_time = datetime.now() - timedelta(minutes=2)
 
     for index, ebay_item in enumerate(ebay_items):
         item_id = ebay_item['item_id']
@@ -111,6 +58,61 @@ def updateSpecificPartFromEbayItems(store, attributes=[]):
         location = ebay_item['location']
         status = ebay_item['status']
 
+        # Deletes ebay item if exists
+        cur.execute(f"""
+        DELETE FROM items WHERE item_id={item_id}
+        """)
+
+        cur.execute(f"""
+        INSERT INTO items VALUES ({item_id}, '{title}', '{listed_date}', '{datetime.now()}', {price}, '{item_url}', '{image_url}', '{location}', '{status}', {store_id});
+        """)
+        print(f"Inserted {index+1} / {len(ebay_items)} into items database. item id: {item_id}")
+
+    print(f"Finished inserting {len(ebay_items)} into database.")  
+
+    # Ebay glitch where some items aren't updated but they weren't sold. Double check these items to see if they sold or not.
+    # areSoldItems() returns a hashMap instead of boolean because I didn't want to waste api calls to ebay api.
+    res = cur.execute(f"""
+    SELECT item_id FROM items WHERE date_last_updated<'{cur_updated_time}' and store_id={store_id} and status="Active"
+    """)
+    res = [item_id[0] for item_id in res.fetchall()]
+    sold_items_map = areSoldItems(res)
+    if sold_items_map:
+        for ebay_id, bool_val in sold_items_map.items():
+            if bool_val:
+                # Update items that weren't updated recently, into "sold" status.
+                cur.execute(f"""
+                UPDATE items 
+                SET status="Sold"
+                WHERE item_id={ebay_id} and store_id={store_id}
+                """)
+
+    con.commit()
+    con.close()
+
+def updateSpecificPartFromEbayItems(store, attributes=[]):
+    # Calls getAllEbayDataFromStores() and will update attributes of the ebay item provided by the arguments. Will not update date_last_updated unless specified.
+    # params: attributes => [Strings] list of strings. Strings will be the attribute name of the items table to be updated.
+    # Return nothing. Updates database. 
+
+    sample_attributes = ['item_id', 'title', 'listed_date', 'date_last_updated', 'price', 'item_url', 'image_url', 'location', 'status', 'store_id']
+
+    if not attributes:
+        print("empty attributes, please add database items attributes to attributes list.")
+        return False
+
+    for attribute in attributes:
+        if attribute not in sample_attributes:
+            print(f"Attribute: {attribute} not in sample_attributes list.")
+            return False
+
+    # Connecting to database.
+    con = sqlite3.connect(DATABASE)
+    cur = con.cursor()
+
+    ebay_items = getAllEbayDataFromStores(store)
+
+    for index, ebay_item in enumerate(ebay_items):
         sql_query = "UPDATE items SET "
 
         for attribute in attributes:
@@ -134,3 +136,4 @@ if __name__ == "__main__":
     # print(getIdFromStoreName("PARTS THAT FIT LLC"))
     # updateSpecificPartFromEbayItems("Basset Auto Wreckers", ['item_url'])
     # print(datetime.now())
+    # pass
